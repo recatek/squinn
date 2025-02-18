d9a40db787761b4b92196a223a1a3896cb108ffb
recatek <recatek@hotmail.com>
2025-02-17 22:41:30 -0500

    Adding a const array for the settings frame data because it will never change

--- server_wtransport/src/settings.rs
+++ server_wtransport/src/settings.rs
@@ -1,7 +1,15 @@
-use quinn_proto::{Connection, Dir, StreamId};
 use std::io::Cursor;
+
+use quinn_proto::{Connection, Dir, StreamId};
 use thiserror::Error;
 use web_transport_proto::{Settings as ProtoSettings, SettingsError as ProtoSettingsError};
+
+/// Pre-encoded settings frame with enable_webtransport set to 1.
+/// Validated in debug at runtime with check_settings_encoded().
+const SETTINGS_ENCODED: [u8; 31] = [
+    0, 4, 28, 171, 96, 55, 67, 1, 128, 255, 210, 119, 1, 8, 1, 51, 1, 192, 0, 0, 0, 198, 113, 112,
+    106, 1, 171, 96, 55, 66, 1,
+];
 
 #[derive(Error, Debug, Clone)]
 pub enum SettingsError {
@@ -26,13 +34,12 @@ pub struct Settings {
     recv_done: bool,
     recv_id: Option<StreamId>,
     recv_data: Vec<u8>,
-
-    settings_data: &'static [u8],
 }
 
-// https://github.com/kixelated/web-transport-rs/blob/main/web-transport-quinn/src/settings.rs#L27
-impl Settings {
-    pub fn new(settings_data: &'static [u8]) -> Self {
+impl Default for Settings {
+    fn default() -> Self {
+        debug_assert!(check_settings_encoded());
+
         Settings {
             send_done: false,
             send_id: None,
@@ -41,19 +48,11 @@ impl Settings {
             recv_done: false,
             recv_id: None,
             recv_data: Vec::new(), // TODO: Add a capacity here
-
-            settings_data,
         }
     }
+}
 
-    pub fn init_settings_data() -> &'static [u8] {
-        let mut settings = ProtoSettings::default();
-        settings.enable_webtransport(1);
-        let mut buf = Vec::<u8>::new();
-        settings.encode(&mut buf);
-        Box::leak(buf.into_boxed_slice()).as_ref()
-    }
-
+impl Settings {
     pub fn update(&mut self, connection: &mut Connection) -> Result<bool, SettingsError> {
         if self.send_done == false {
             self.send_done |= self.try_send(connection)?;
@@ -72,14 +71,13 @@ impl Settings {
         }
 
         if let Some(send_id) = self.send_id {
-            let sent = connection
-                .send_stream(send_id)
-                .write(&self.settings_data[self.send_bytes..])?;
-            self.send_bytes += sent;
+            let mut send_stream = connection.send_stream(send_id);
+            self.send_bytes += send_stream.write(&SETTINGS_ENCODED[self.send_bytes..])?;
 
-            if self.send_bytes >= self.settings_data.len() {
-                return Ok(true);
-            }
+            return match self.send_bytes >= SETTINGS_ENCODED.len() {
+                true => Ok(true),
+                false => Ok(false), // Keep trying
+            };
         }
 
         Ok(false) // Keep trying
@@ -92,24 +90,33 @@ impl Settings {
 
         if let Some(recv_id) = self.recv_id {
             let mut recv_stream = connection.recv_stream(recv_id);
-            let mut chunks = recv_stream
+            let recv_chunk = recv_stream
                 .read(true)
-                .map_err(|_| SettingsError::UnexpectedEnd)?;
-            let chunk = chunks
+                .map_err(|_| SettingsError::UnexpectedEnd)?
                 .next(usize::MAX)?
                 .ok_or(SettingsError::UnexpectedEnd)?;
-            self.recv_data.extend_from_slice(&chunk.bytes);
+            self.recv_data.extend_from_slice(&recv_chunk.bytes);
 
             return match ProtoSettings::decode(&mut Cursor::new(&self.recv_data)) {
                 Ok(settings) => match settings.supports_webtransport() {
                     1 => Ok(true), // We're done!
                     _ => Err(SettingsError::WebTransportUnsupported),
                 },
-                Err(ProtoSettingsError::UnexpectedEnd) => Ok(false), // Keep reading
+                Err(ProtoSettingsError::UnexpectedEnd) => Ok(false), // Keep trying
                 Err(e) => Err(e.into()),
             };
         }
 
         Ok(false) // Keep trying
     }
+}
+
+fn check_settings_encoded() -> bool {
+    let mut settings = ProtoSettings::default();
+    settings.enable_webtransport(1);
+
+    let mut buf = Vec::<u8>::new();
+    settings.encode(&mut buf);
+
+    &buf == &SETTINGS_ENCODED
 }
